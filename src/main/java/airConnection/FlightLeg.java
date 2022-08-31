bec14e01adee8756d0cf7d1aa3780f7a20a953be
slawo <slawekpettke@gmail.com>
2022-08-31 13:29:26 +0200

    pierwsza klasa

--- src/main/java/airConnection/FlightLeg.java
+++ src/main/java/airConnection/FlightLeg.java
@@ -1,4 +1,58 @@
 package airConnection;
 
 public class FlightLeg {
+
+    private String from;
+    private String to;
+    boolean delayed = false;
+    private int price;
+
+    private FlightLeg(FlightLegBuilder flightLegBuilder) {
+        this.from = flightLegBuilder.from;
+        this.to = flightLegBuilder.to;
+        this.price = flightLegBuilder.price;
+    }
+
+
+    public void setDelayed(boolean delayed) {
+        this.delayed = delayed;
+    }
+
+    @Override
+    public String toString() {
+        return "FlightLeg{" +
+                "from='" + from + '\'' +
+                ", to='" + to + '\'' +
+                ", price=" + price +
+                ", delayed=" + delayed +
+                '}';
+    }
+
+    public static class FlightLegBuilder{
+        private String from;
+        private String to;
+        boolean delayed = false;
+        private int price;
+
+        public FlightLeg build(){
+            return new FlightLeg(this);
+        }
+
+        public FlightLegBuilder buildFrom(String from){
+            this.from = from;
+            return this;
+        }
+
+        public FlightLegBuilder buildTo(String to){
+            this.to = to;
+            return this;
+        }
+
+        public FlightLegBuilder buildPrice(int price){
+            this.price = price;
+            return this;
+        }
+
+    }
 }
+
